6cbcdd6ce5850c595761872e82f3867a0551379f
luka-danger <lukathevegan@gmail.com>
2023-12-26 09:15:28 -0700

    Create query selector variables

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operations]');
+const equalsButton = document.querySelector('[data-equals]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+const previousTextElement = document.querySelector('[data-previous]');
+const currentTextElement = document.querySelector('[data-current]');
+
+
+
 const add = function(a, b) {
 	return a + b;
 };
