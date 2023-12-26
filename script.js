c6a00998332387cf19659f9cacc22f9b7f8ab382
luka-danger <lukathevegan@gmail.com>
2023-12-26 07:33:18 -0700

    Create operator function
    Use switch case to call arithmetic funcitons with symbols. Untested function

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ const divide = function(a, b) {
     return a / b; 
   };
 
+const operate = (operator, a, b) {
+    let a = Number(a);
+    let b = Number(b); 
+    switch(operator) {
+        case +:
+            return add(a, b); 
+        case -: 
+            return subtract(a, b);
+        case *:
+            return multiply(a, b); 
+        case /: 
+            if (b === 0) {
+                return undefined
+            } else {
+                return divide(a, b);
+            }
+    }
+}; 
